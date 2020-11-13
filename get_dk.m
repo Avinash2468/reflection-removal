596170eb4e1e7e9880ae28fe1c4de37ad0da93a2
Mallika <mallika2011@yahoo.com>
2020-11-13 17:57:00 +0530

    Update dk computation

--- get_dk.m
+++ get_dk.m
@@ -1,17 +1,28 @@
 function [dx,dy] = get_dk(im, thresh)
     
+    im=im2double(im);
     im = rgb2gray(im);
     [ht,wd] = size(im);
-
+    
     %compute the laplacian of the grayscale image
     laplacianKernel = [0 -1 0; -1 4 -1; 0 -1 0];
-    laplacianImage = imfilter((im), laplacianKernel);
+    laplacianImage = imfilter((im), laplacianKernel); 
     
     %compute the 2D autocorrelation map of the laplacian
     auto = xcorr2(laplacianImage);  
-    bdry = 1;
-    auto = auto(bdry:end-bdry, bdry:end-bdry);
     [m,n] = size(auto);
+    ori_x = floor(m/2)+1;
+    ori_y = floor(n/2)+1;
+        
+    
+    %view autocorrelation map
+    
+    auto2 = uint8(255 * mat2gray(auto));   
+    x=[-floor(m/2)+1 floor(m/2)+1]; y=[-floor(n/2)+1 floor(n/2)+1];
+    imagesc(x,y,auto2);
+    imshow(auto2)
+    axis on
+    
     
     %compute local maxima in 5x5 patch
     %first and second local minima
@@ -20,13 +31,14 @@ function [dx,dy] = get_dk(im, thresh)
 
     
     %remove local maxima within 4 pixels of the origin
-    max_1(floor(m/2)+1-4 : floor(m/2)+1+4 , floor(n/2)+1-4 : floor(n/2)+1+4) = 0;
-    max_2(floor(m/2)+1-4 : floor(m/2)+1+4 , floor(n/2)+1-4 : floor(n/2)+1+4) = 0;
+    max_1(ori_x-4 : ori_x+4 , ori_y-4 : ori_y+4) = 0;
+    max_2(ori_x-4 : ori_x+4 , ori_y-4 : ori_y+4) = 0;
 
     
-    max_indices = find(((max_1 - max_2) > thresh));
+    max_indices = find(max_1==auto & ((max_1 - max_2) > thresh));
     max_vals = max_1(max_indices);
 
+    
     
     maxima = -1;
     dx = 0; 
@@ -41,11 +53,13 @@ function [dx,dy] = get_dk(im, thresh)
                 maxima = max_vals(i);
         end
     end
-
+    
+    dx
+    dy
+    
     %origin shift for the coordinates
-    dx = abs (dx - (floor(n/2) + 1));
-    dy = abs (dy - (floor(m/2) + 1));
-
+    dx = abs (dx - ori_y);
+    dy = abs (dy - ori_x);
     
 end
     
