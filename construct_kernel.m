0afa4ed3d9d7047d87bc7fb6cd56f05ce6a0627d
Fiza <fizahusain1110@gmail.com>
2020-11-14 19:39:06 +0530

    minor updates

--- construct_kernel.m
+++ construct_kernel.m
@@ -1,36 +1,28 @@
 function [kernel] = construct_kernel(h, w, dx, dy, c)
-
-file = fopen(‘dk.txt’, ‘r’);
-format = ‘%f %f’;
-A = fscanf(file, format);
-fclose(file);
-file = fopen(‘ck.txt’, ‘r’);
-format = ‘%f’;
-A = fscanf(file, format);
-fclose(file);
-
+  
 index = [1:h*w];
 index = reshape(index, [h, w]);
 
 % shift pixels by dk
-index2 = padarray(index, abs([dy, dx]));
-index2 = circshift(index2, [dy, dx]);
-index2 = index2(abs(dy) + 1:end - abs(dy), abs(dx) + 1:end - abs(dx), :);
+neigh_index1 = padarray(index, abs([dy, dx]));
+neigh_index1 = circshift(neigh_index1, [dy, dx]);
+neigh_index1 = neigh_index1(abs(dy) + 1:end - abs(dy), abs(dx) + 1:end - abs(dx), :);
 
-% applying attenuation on shifted pixels.
+% applying attenuation on shifted pixels
+
 indexes = ones(h, w);
 
-% pixels having a shift and pixels to which they are getting mapped.
-shifted_pixels = sparse(index(:), index(:), indexes);
+% pixels having a shift and pixels to which they are getting mapped
+shifted_pixels1 = sparse(index(:), index(:), indexes);
 
 indexes2 = ones(h, w);
-indexes2(index2 == 0) = 0;
+indexes2(neigh_index1 == 0) = 0;
 indexes2 = indexes2.*c;
 
 % pixels which have undergone attenuation
-shifted_pixels2 = sparse(index(:), index(:), indexes2);
+neigh_index2 = circshift(index, [dy dx]);
+shifted_pixels2 = sparse(index(:), neigh_index2(:), indexes2);
 
-kernel = shifted_pixels + shifted_pixels2;
+kernel = shifted_pixels1 + shifted_pixels2;
 
 end
-
